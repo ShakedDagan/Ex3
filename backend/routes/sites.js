4ecb62cc0f4c29fa0003672587a3d419e6bf2c4c
Shaked Dagan <49585133+ShakedDagan@users.noreply.github.com>
2022-05-14 22:11:30 +0300

    Add files via upload

--- backend/routes/sites.js
+++ backend/routes/sites.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const Site = require('../models/site')
+const fs = require('fs')
 
 // Getting all
 router.get('/', async (req, res) => {
@@ -18,33 +19,74 @@ router.get('/:name', getSite, (req, res) => {
 })
 
 // Creating one
-router.post('/', async (req, res) => {
-  console.log(req.body)
-  // const site = new Site({
-  //   name: req.body.name,
-  //   state: req.body.state,
-  //   town: req.body.town,
-  //   year: req.body.year,
-  //   data1: req.body.data1,
-  //   data2: req.body.data2,
-  //   img1: req.body.img1,
-  //   img2: req.body.img2,
-  //   img3: req.body.img3
-    
-  // })
-  // try {
-  //   const newSite = await site.save()
-  //   res.status(201).json(newSite)
-  // } catch (err) {
-  //   res.status(400).json({ message: err.message })
-  // }
+const multer  = require('multer')
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    let dir = './assets/'+req.body.name;
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir, {recursive:true});
+    }
+  
+    cb(null, './assets/'+req.body.name);
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '_' + file.originalname);
+  }
 })
+const upload = multer({ storage:storage })
+const cpUpload = upload.fields([{ name: 'img1', maxCount: 1 }, { name: 'img2', maxCount: 1 }, {name: 'img3', maxCount:1}])
+router.post('/', cpUpload, async function (req, res) {
+   // req.file is the name of your file in the form above, here 'uploaded_file'
+   // req.body will hold the text fields, if there were any 
+   if(!req.body.name || !req.body.state || !req.body.town || !req.body.year || !req.body.data1 || !req.body.data2 ||
+    !req.files.img1 || !req.files.img2 || !req.files.img3) {
+      res.send("<h2>Some Data is missing in the form.</h2><h2>Please try again.</h2>")
+      return;
+    }
+   let siteCheck = await Site.find({name:req.body.name})
+   if(siteCheck != null){
+    res.send("<h2>Site name already exists in the database.</h2>")
+    return;
+   }
+   let img1 = "http://localhost:3000/images/"+req.body.name+'/'+req.files.img1[0].filename;
+   let img2 = "http://localhost:3000/images/"+req.body.name+'/'+req.files.img2[0].filename;;
+   let img3 = "http://localhost:3000/images/"+req.body.name+'/'+req.files.img3[0].filename;;
+   const site = new Site({
+    name: req.body.name,
+    state: req.body.state,
+    town: req.body.town,
+    year: req.body.year,
+    data1: req.body.data1,
+    data2: req.body.data2,
+    img1: img1,
+    img2: img2,
+    img3: img3
+    
+  })
+  try {
+    const newSite = await site.save()
+    //res.status(201).json(newSite)
+    res.send("<script>window.close();</script > ")
+  } catch (err) {
+    res.status(400).json({ message: err.message })
+  }
+});
 
 
 // Deleting One
 router.delete('/:name', getSite, async (req, res) => {
   try {
     await res.site[0].remove()
+    //console.log(res.site[0]);
+    let dir = './assets/'+res.site[0].name;
+    console.log(dir)
+    fs.rm(dir, { recursive: true }, (err) => {
+      if (err) {
+          throw err;
+      }
+  
+      console.log(`${dir} is deleted!`);
+  });
     res.json({ message: 'Deleted Site' })
   } catch (err) {
     res.status(500).json({ message: err.message })
--- backend/server.js
+++ backend/server.js
@@ -10,11 +10,12 @@ db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
 
 const cors = require('cors');
-app.use(cors({origin: 'http://localhost:5500'}));
+app.use(cors({origin: '*'}));
 
 app.use(express.json())
 const sitesRouter = require('./routes/sites')
 app.use('/sites', sitesRouter)
 app.use('/images',express.static(__dirname + '/assets'))
+
 
 app.listen(process.env.PORT, () => console.log('Server Started'))
